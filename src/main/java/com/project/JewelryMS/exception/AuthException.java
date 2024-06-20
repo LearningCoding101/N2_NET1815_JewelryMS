72b3b6a1897666be0b7cf678e363b3d315368ce4
LearningCoding101 <hahoang33322@gmail.com>
2024-06-20 20:23:21 +0700

    temp

--- src/main/java/com/project/JewelryMS/exception/AuthException.java
+++
@@ -1,8 +0,0 @@
-package com.project.JewelryMS.exception;
-
-public class AuthException extends RuntimeException{
-
-    public AuthException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/project/JewelryMS/model/OrderDetail/OrderTotalRequest.java
+++
@@ -1,9 +0,0 @@
-package com.project.JewelryMS.model.OrderDetail;
-
-import lombok.Data;
-
-@Data
-public class OrderTotalRequest {
-    Float subTotal;
-    Float DiscountProduct;
-}
