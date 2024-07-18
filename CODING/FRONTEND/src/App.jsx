5f72cca6eda897d24eb97f5db045423232b3692e
LearningCoding101 <hahoang33322@gmail.com>
2024-07-18 06:14:28 +0000

    Sync repositories and remove sensitive file

--- CODING/FRONTEND/src/App.jsx
+++ CODING/FRONTEND/src/App.jsx
@@ -65,6 +65,8 @@ import InactiveProductSell from "./page/manager/inactiveProductSell/InactiveProd
 import InactiveProductSellAdmin from "./page/manager/inactiveProductSell/InactiveProductSell";
 import ProductBuyManager from "./page/manager/productbuy/ProductBuyManager";
 import AppraisalPage from "./page/staff/staff-appraise/Appraise";
+import ProductBuyInactive from "./page/manager/productbuy/ProductBuyInactive";
+import Refund from "./component/Refund/Refund";
 
 const PrivateProute = ({ role }) => {
   console.log(role);
@@ -121,6 +123,9 @@ function App() {
           {/* <Route path="transaction/detail/:orderID/:orderType" element={<TransactionDetail />} /> */}
           salecomparision
           <Route path="salecomparision" element={<SaleComparision />} />
+          <Route path="transaction/detail/:orderID/OUTGOING" element={<Transaction_ProductBuy />} />
+          <Route path="transaction/detail/:orderID/INGOING" element={<Transaction_ProductSell />} />
+          <Route path="check/guarantee" element={<CheckGuarantee />} />
           <Route
             path="transaction/detail/:orderID/OUTGOING"
             element={<Transaction_ProductBuy />}
@@ -129,12 +134,10 @@ function App() {
             path="transaction/detail/:orderID/INGOING"
             element={<Transaction_ProductSell />}
           />
-          <Route path="check/guarantee" element={<CheckGuarantee />} />
-          <Route path="transaction/detail/:orderID/OUTGOING" element={<Transaction_ProductBuy />} />
-          <Route path="transaction/detail/:orderID/INGOING" element={<Transaction_ProductSell />} />
           <Route path="staff/range" element={<StaffPerformanceRange />} />
           <Route path="inactive/product" element={<InactiveProductSell />} />
           <Route path="productbuy" element={<ProductBuyManager />} />
+          <Route path="productbuy/inactive" element={<ProductBuyInactive />} />
         </Route>
       </Route>
       <Route path="admin" element={<PrivateProute role="ROLE_ADMIN" />}>
@@ -152,6 +155,12 @@ function App() {
           <Route path="topproductsell" element={<TopProductSells />} />
           <Route path="statictic/account" element={<Statistic_Account />} />
           <Route path="inactive/product" element={<InactiveProductSellAdmin />} />
+          <Route path="productbuy" element={<ProductBuyManager />} />
+          <Route path="productbuy/inactive" element={<ProductBuyInactive />} />
+          <Route
+            path="inactive/product"
+            element={<InactiveProductSellAdmin />}
+          />
         </Route>
       </Route>
       <Route path="staff" element={<PrivateProute role={"ROLE_STAFF"} />}>
@@ -168,6 +177,7 @@ function App() {
           <Route path="trace/by-date" element={<ViewGuaranteeByDate />} />
           <Route path="chat" element={<ChatBox />} />
           <Route path="appraise" element={<AppraisalPage />} />
+          <Route path="order/refund" element={<Refund />} />
 
           <Route path="product" element={<StaffProduct />} />
           <Route path="changepassword" element={<StaffChangePassword />} />
